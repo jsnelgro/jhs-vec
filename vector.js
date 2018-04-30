e0260338f60cdb483f3197ad6598f2acc68292ae
John Snelgrove <johnny.snelgrove@gmail.com>
2018-04-30 16:37:12 -0700

    init

---
+++ vector.js
@@ -0,0 +1,169 @@
+export class Vec extends Array {
+  constructor(x, y, z, ...rest) {
+    super()
+    if (Array.isArray(x)) {
+      var [x, y, z, ...rest] = x
+    }
+    if (x !== undefined) this[0] = this.x = x
+    if (y !== undefined) this[1] = this.y = y
+    if (z !== undefined) this[2] = this.z = z
+    rest.forEach((v) => (v !== undefined ? this.push(v) : null))
+    // BUG: get and set syntax aren't being transpiled correctly
+    // need to use this approach instead
+    Object.defineProperties(this, {
+      x: {
+        get: () => this[0] || 0,
+        set: (v) => (this[0] = v),
+      },
+      y: {
+        get: () => this[1] || 0,
+        set: (v) => (this[1] = v),
+      },
+      z: {
+        get: () => this[2] || 0,
+        set: (v) => (this[2] = v),
+      },
+    })
+  }
+
+  map = (fn, thisArg = this) => {
+    return new Vec(super.map(fn, thisArg))
+  }
+
+  filter = (fn, thisArg = this) => {
+    let filterRes = super.map((el) => fn(el), thisArg).map((el, i) => (el ? this[i] : 0))
+    return new Vec(filterRes)
+  }
+
+  reduce = (...args) => {
+    let res = super.reduce(...args)
+    return Array.isArray(res) ? new Vec(res) : res
+  }
+
+  toArray = () => {
+    return [...this]
+  }
+}
+
+export const Vector = (...dimensions) => {
+  return new Vec(...dimensions)
+}
+
+export const create = Vector
+
+const argsOrArray = (...vecs) => {
+  let res = vecs.length === 1 && Array.isArray(vecs[0]) ? vecs[0] : vecs
+
+  // TODO: THIS MUST BE WHY WE NEED TYPESCRIPT
+  // if (typeof res[0] === 'number') {
+  //   res = [res]
+  // }
+  return res
+}
+
+export const add = (...vecs) => {
+  vecs = argsOrArray(...vecs)
+  let scalars = 0
+  while (typeof vecs[vecs.length - 1] === 'number') {
+    scalars += vecs.pop()
+  }
+  if (vecs.length === 0) {
+    return scalars
+  }
+  let res = vecs.reduceRight((acc, v) => {
+    if (typeof v === 'number') {
+      scalars += v
+      return acc
+    }
+    if (acc.length !== v.length) {
+      throw 'Vectors must be equal length or a scalar'
+    }
+    return Vector(acc.map((d, i) => d + (v[i] === undefined ? 1 : v[i])))
+  })
+  return typeof res === 'number' ? res : Vector(res.map((d) => d + scalars))
+}
+
+export const sub = (...vecs) => {
+  vecs = argsOrArray(...vecs)
+  if (typeof vecs[0] === 'number') {
+    throw 'Cannot subtract a vector from a scalar'
+  }
+  return vecs.reduce((acc, v) => {
+    if (typeof v === 'number') {
+      return Vector(acc.map((d) => d - v))
+    }
+    return Vector(acc.map((d, i) => d - v[i]))
+  })
+}
+
+export const mult = (...vecs) => {
+  vecs = argsOrArray(...vecs)
+  let scalars = 1
+  while (typeof vecs[vecs.length - 1] === 'number') {
+    scalars *= vecs.pop()
+  }
+  if (vecs.length === 0) {
+    return scalars
+  }
+  let res = vecs.reduceRight((acc, v) => {
+    if (typeof v === 'number') {
+      scalars *= v
+      return acc
+    }
+    if (acc.length !== v.length) {
+      throw 'Vectors must be equal length or a scalar'
+    }
+    return Vector(acc.map((d, i) => d * (v[i] === undefined ? 1 : v[i])))
+  })
+  return typeof res === 'number' ? res : Vector(res.map((d) => d * scalars))
+}
+
+export const div = (...vecs) => {
+  vecs = argsOrArray(...vecs)
+  if (typeof vecs[0] === 'number') {
+    throw 'Cannot divide a scalar by a vector'
+  }
+  return vecs.reduce((acc, v) => {
+    if (typeof v === 'number') {
+      return Vector(acc.map((d) => d / v))
+    }
+    return Vector(acc.map((d, i) => d / v[i]))
+  })
+}
+
+export const sum = (...vecs) => {
+  return add(vecs).reduce((acc, d) => acc + d)
+}
+
+export const mag = (vec) => {
+  let z = vec.z ** 2 || 0
+  return Math.sqrt(sum(vec))
+  return Math.sqrt(vec.x ** 2 + vec.y ** 2 + z)
+}
+
+export const magnitude = mag
+
+export const norm = (vec) => {
+  return mag(vec) > 0 ? div(vec, mag(vec)) : vec
+}
+
+export const centroid = (...vecs) => {
+  return div(add(...vecs), vecs.length)
+}
+
+const zip = (...rows) => [...rows[0]].map((_, c) => rows.map((row) => row[c]))
+export const distance = (vec1, vec2) => {
+  return Math.sqrt(
+    zip(vec2Array(vec2), vec2Array(vec1))
+      .map(([d1, d2]) => (d1 - d2) ** 2)
+      .reduce((acc, x) => acc + x)
+  )
+}
+
+export const array2Vec = (arr) => Vector(...arr)
+export const a2v = array2Vec
+
+export const vec2Array = (vec) => {
+  return [vec.x, vec.y, vec.z ? vec.z : 0]
+}
+export const v2a = vec2Array
---
+++ vector.test.js
@@ -0,0 +1,257 @@
+import * as veclib from './vector3d.js'
+
+describe('Vector class', () => {
+  const { Vec } = veclib
+
+  test('vector should have x, y, and z properties', () => {
+    let v = new Vec(1, 2, 3)
+    expect(v).toHaveProperty('x')
+    expect(v).toHaveProperty('y')
+    expect(v).toHaveProperty('z')
+  })
+
+  test('x, y, z should proxy array indices 0, 1, 2', () => {
+    let v = new Vec(4, 5, 6)
+    expect(v.x === v[0] && v.y === v[1] && v.z === v[2]).toBe(true)
+  })
+
+  test('vectors with more or less than 3 dimensions work exactly the same', () => {
+    let vLong = new Vec(1, 2, 3, 4, 5, 6)
+    let vShort = new Vec(1)
+    expect(vLong).toHaveLength(6)
+    expect(vShort).toHaveLength(1)
+  })
+
+  test('mutating x, y, or z should update index and vise versa', () => {
+    let v = new Vec(4, 5, 6)
+    v.x = 8
+    v[1] = 9
+    expect(v[0] === 8 && v.y === 9).toBe(true)
+  })
+
+  describe('map, filter, reduce', () => {
+    test('map returns a vector instance instead of a vanilla array', () => {
+      let v = new Vec(5, 6, 7)
+      let m = v.map((el) => el * 2)
+      expect(m.x).toEqual(m[0])
+      expect(m.x).toEqual(10)
+    })
+
+    test('filter sets false test values to zero', () => {
+      let v = new Vec(5, 6, 7)
+      let m = v.filter((el) => el > 5)
+      expect(m.x).toBeDefined()
+      expect(m.x).toEqual(0)
+    })
+
+    test('reduce should behave exactly like native Array.reduce', () => {
+      let v = new Vec([4, 5, 6])
+      expect(v.reduce((acc, d) => acc + d)).toBe(15)
+      expect(v.reduce((acc, d) => ({ ...acc, [d]: true }), {})).toEqual({
+        4: true,
+        5: true,
+        6: true,
+      })
+    })
+
+    test('if reduce returns an array, it should be cast to a Vector', () => {
+      let v = new Vec(2, 3, 4)
+      let res = v.reduce((acc, d) => [...acc, d * 2], [])
+      let resLong = v.reduce((acc, d) => [...acc, d, d * 2], [])
+      expect(res).toHaveProperty('x')
+      expect(resLong).toHaveProperty('x')
+    })
+  })
+
+  test('vector is JSON serializable into an array', () => {
+    let v = new Vec(4, 5, 6)
+    expect(JSON.stringify(v) === JSON.stringify([4, 5, 6])).toBe(true)
+  })
+
+  test('An existing array can be "vectorized"', () => {
+    let v = new Vec([3, 4, 5, 6])
+    expect(v[0] === 3 && v[1] === 4 && v[2] === 5 && v[3] === 6).toBe(true)
+  })
+
+  test('a vector can be cast back into a plain array with toArray()', () => {
+    let v = new Vec([3, 4, 5])
+    expect(v.toArray()).not.toHaveProperty('x')
+    expect(v).toHaveProperty('x')
+  })
+})
+
+describe('static vector methods', () => {
+  const { Vec, Vector } = veclib
+  describe('add', () => {
+    const { add } = veclib
+    test('if an array is passed in, it is cast to a vector', () => {
+      let v = [2, 3, 4]
+      let res = add(v, 5)
+      expect(res).toBeInstanceOf(Vec)
+    })
+
+    test('can add a vector and a scalar', () => {
+      let v = [2, 3, 4]
+      let res = add(v, 5)
+      expect(res).toHaveProperty('x', 7)
+      expect(res).toHaveProperty('y', 8)
+      expect(res).toHaveProperty('z', 9)
+      let res2 = add(3, v)
+      expect(res2.toArray()).toEqual([5, 6, 7])
+    })
+
+    test('trying to add a single vector returns the vector', () => {
+      expect(add([1, 2, 3]).toArray()).toEqual([1, 2, 3])
+    })
+
+    test('can add a scalars together', () => {
+      expect(add(1, 2)).toBe(3)
+      expect(add(1, 2, 3)).toBe(6)
+    })
+
+    test('adds two vectors together', () => {
+      let v1 = [2, 3, 4]
+      let v2 = [1, 2, 3]
+      let res = add(v1, v2)
+      expect(res.toArray()).toEqual([3, 5, 7])
+    })
+
+    test('adds n vectors together', () => {
+      let v1 = [2, 3, 4]
+      let v2 = [2, 3, 4]
+      let v3 = [2, 3, 4]
+      let v4 = [2, 3, 4]
+      expect(add(v1, v2, v3, v4).toArray()).toEqual([8, 12, 16])
+    })
+
+    test('can add 2d vecs together', () => {
+      let vecs = [[3, 3], [3, 3]]
+      expect(add(vecs).toArray()).toEqual([6, 6])
+    })
+
+    test('trying to add different sized vecs together throws an error', () => {
+      let vecs = [[3, 3], [3, 3, 3], 3]
+      expect(() => add(vecs)).toThrow()
+    })
+
+    test('can add n-dimensional vecs together', () => {
+      let vecs = [3, [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
+      expect(add(vecs).toArray()).toEqual([6, 6, 6, 6, 6])
+    })
+
+    test('can pass vectors in either an array or as arguments', () => {
+      let vecs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+      expect(add(vecs).toArray()).toEqual([12, 15, 18])
+    })
+  })
+
+  describe('subtract', () => {
+    const { sub } = veclib
+    test('if an array is passed in, it is cast to a vector', () => {
+      let v = [2, 3, 4]
+      let res = sub(v, 5)
+      expect(res).toBeInstanceOf(Vec)
+    })
+
+    test('can pass vectors in either an array or as arguments', () => {
+      let vecs = [[5, 2, 3], [4, 5, 6], [1, 8, 9]]
+      expect(sub(vecs).toArray()).toEqual([0, -11, -12])
+    })
+
+    test('processes args from left to right', () => {
+      let v = [1, 1, 1]
+      expect(sub(v, 1).toArray()).toEqual([0, 0, 0])
+    })
+
+    test('trying to subtract a vector from a scalar should throw an error', () => {
+      let v = [1, 1, 1]
+      expect(() => sub(1, v)).toThrow()
+    })
+  })
+
+  describe('multiply', () => {
+    const { mult } = veclib
+    test('if an array is passed in, it is cast to a vector', () => {
+      let v = [2, 3, 4]
+      let res = mult(v, 5)
+      expect(res).toBeInstanceOf(Vec)
+    })
+
+    test('can multiply a vector and a scalar', () => {
+      let v = [2, 3, 4]
+      let res = mult(v, 2)
+      expect(res).toHaveProperty('x', 4)
+      expect(res).toHaveProperty('y', 6)
+      expect(res).toHaveProperty('z', 8)
+      let res2 = mult(0, v)
+      expect(res2.toArray()).toEqual([0, 0, 0])
+    })
+
+    test('can multiply a scalars together', () => {
+      expect(mult(1, 2)).toBe(2)
+      expect(mult(1, 2, 3)).toBe(6)
+    })
+
+    test('multiplies two vectors together', () => {
+      let v1 = [2, 3, 4]
+      let v2 = [1, 2, 3]
+      let res = mult(v1, v2)
+      expect(res.toArray()).toEqual([2, 6, 12])
+    })
+
+    test('multiply n vectors together', () => {
+      let v1 = [1, 0, 2]
+      let v2 = [1, 3, 2]
+      let v3 = [1, 3, 2]
+      let v4 = [1, 3, 2]
+      expect(mult(v1, v2, v3, v4).toArray()).toEqual([1, 0, 16])
+    })
+
+    test('can multiply 2d vecs together', () => {
+      let vecs = [[3, 3], [3, 3]]
+      expect(mult(vecs).toArray()).toEqual([9, 9])
+    })
+
+    test('trying to multiply different sized vecs together throws an error', () => {
+      let vecs = [[3, 3], [3, 3, 3], 3]
+      expect(() => mult(vecs)).toThrow()
+    })
+
+    test('can mult n-dimensional vecs together', () => {
+      let vecs = [3, [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
+      expect(mult(vecs).toArray()).toEqual([3, 3, 3, 3, 3])
+    })
+
+    test('can pass vectors in either an array or as arguments', () => {
+      let vecs = [[1, 2, 2], [1, 1, 0], [1, 1, 9]]
+      expect(mult(vecs).toArray()).toEqual([1, 2, 0])
+    })
+  })
+
+  describe('divide', () => {
+    const { div } = veclib
+    test('if an array is passed in, it is cast to a vector', () => {
+      let v = [2, 3, 4]
+      let res = div(v, 5)
+      expect(res).toBeInstanceOf(Vec)
+    })
+
+    test('can pass vectors in either an array or as arguments', () => {
+      let vecs = [[5, 2, 3], [4, 5, 6], [1, 8, 9]]
+      let res = div(vecs).toArray()
+      let expected = [1.25, 0.05, 0.05555555555555555]
+      res.forEach((val, i) => expect(val).toBeCloseTo(expected[i]))
+    })
+
+    test('processes args from left to right', () => {
+      let v1 = [10, 4, 6]
+      let v2 = [2, 2, 2]
+      expect(div(v1, 1, v2).toArray()).toEqual([5, 2, 3])
+    })
+
+    test('trying to subtract a vector from a scalar should throw an error', () => {
+      let v = [1, 1, 1]
+      expect(() => sub(1, v)).toThrow()
+    })
+  })
+})
